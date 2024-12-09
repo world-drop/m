0fea62941fcea917fb68daeb6ee696688f07b814
Max <dooksiep33@gmail.com>
2024-12-09 14:16:22 -0500

    Initial Commit

---
+++ m/mApp.swift
@@ -0,0 +1,17 @@
+//
+//  mApp.swift
+//  m
+//
+//  Created by Maxwell Powell (student LM) on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct mApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
